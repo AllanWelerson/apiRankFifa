284366164a20ef23ae649cece3fb737c0367dcee
= <allanwelerson@gmail.com>
2019-01-29 00:18:45 -0200

    permission

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended : false }));
 app.use((req, res, next)=>{
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods','GET');
